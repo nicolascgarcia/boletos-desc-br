50869a717c53e4ce450e571f5d192c00a10310ac
Nicolas Carvalho Garcia <ni.car.garcia@gmail.com>
2020-06-18 15:10:43 -0300

    teste

---
+++ src/boleto.js
@@ -0,0 +1,14 @@
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
+
+
+  exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
+  exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
+  exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }

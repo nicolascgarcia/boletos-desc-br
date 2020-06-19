6cebd430ae9d41fed2428979c17295832743aa90
Nicolas Carvalho Garcia <ni.car.garcia@gmail.com>
2020-06-19 10:16:22 -0300

    arquivo teste

---
+++ teste.js
@@ -0,0 +1,16 @@
+const boletos = require('./src/index') // Importando o módulo e atribuindo numa constante
+
+// Número do boleto para teste
+// O número pode conter pontos e espaços pois serão tratados
+let boletocodigo = "836800000025147400220108001010202057307178387006"
+
+// Retorna true ou false se o codigo do boleto é válido ou não
+let validacao = boletos.boleto(boletocodigo)
+
+// Retorna os dados do boleto em um objeto
+let dados = boletos.validarBoleto(boletocodigo)
+
+// Retornando no terminal todos os dados
+console.log(validacao)
+console.log("\n")
+console.log(dados)

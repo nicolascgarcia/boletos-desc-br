d2505e8aa98acee372b0b7a55ed02efebb56067f
Nicolas Carvalho Garcia <ni.car.garcia@gmail.com>
2020-06-19 11:21:10 -0300

    novo teste

--- teste.js
+++ teste.js
@@ -2,7 +2,7 @@ const boletofunc = require('./src/index') // Importando o módulo e atribuindo n
 
 // Número do boleto para teste
 // O número pode conter pontos e espaços pois serão tratados
-let boletocodigo = "836800000025147400220108001010202057307178387006"
+let boletocodigo = "74891 12024 03943.907380 23105.411054 2 82820000051180"
 
 // Retorna true ou false se o codigo do boleto é válido ou não
 let validacao = boletofunc.validarBoleto(boletocodigo)
